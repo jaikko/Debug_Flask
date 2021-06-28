1012f2165a5139a520eb1d6b2f0969f0ee215ec1
jaikko <65661383+jaikko@users.noreply.github.com>
2021-06-28 16:16:28 +0200

    Add files via upload

---
+++ tests/tests_integration/test_max_points.py
@@ -0,0 +1,37 @@
+import server
+
+
+class Test12MaxPointsCompet:
+
+    def setup(self):
+        server.clubs = [{
+            "name": "Test",
+            "email": "test@test.co",
+            "points": "13"
+        }]
+
+        server.competitions = [{
+            "name": "Spring Festival",
+            "date": "2020-03-27 10:00:00",
+            "numberOfPlaces": "15"
+            },
+            {
+            "name": "Spring",
+            "date": "2020-03-27 10:00:00",
+            "numberOfPlaces": "14"
+        }]
+
+    # nombre de points inférieur  à 12
+    def test_booking(self, client):
+
+        rv = client.post('/purchasePlaces', data=dict(
+            places=4,
+            competition="Spring Festival",
+            club="Test"
+            ))
+        assert rv.status_code == 200
+        html = rv.get_data(as_text=True)
+        print(html)
+        assert '<h2>Welcome, test@test.co </h2>' in html
+        assert 'Points available: 13' in html
+        assert '<li>Great-booking complete!</li>' in html
